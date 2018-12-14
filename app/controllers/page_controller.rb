cf14a143d6d777afa5c875e140f74d7458056c9f
Paolo Sanchez <paolo.sanchez@obf.ateneo.edu>
2018-12-15 01:38:59 +0800

    pota

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,6 @@
 class PageController < ApplicationController
   before_action :require_logged_out, except: [:admin]
+  before_action :require_user, only: [:admin]
   def index
     @sale = Sale.all
   end

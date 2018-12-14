9dcaf1c6fccf47706a397263e3bab7f76174a0df
Paolo Sanchez <paolo.sanchez@obf.ateneo.edu>
2018-12-15 01:46:44 +0800

    pota

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,11 +1,11 @@
 class PageController < ApplicationController
   before_action :require_logged_out, except: [:admin]
   before_action :require_admin, only: [:admin]
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  def record_not_found
-    redirect_to root_path
-  end
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  #
+  # def record_not_found
+  #   redirect_to root_path
+  # end
   def index
     @sale = Sale.all
   end

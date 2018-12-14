0005d6dc935c7f37f3d96cd46e850f0a54210f83
Paolo Sanchez <paolo.sanchez@obf.ateneo.edu>
2018-12-15 01:43:36 +0800

    huhu

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,6 @@
 class PageController < ApplicationController
   before_action :require_logged_out, except: [:admin]
-  before_action :require_user, only: [:admin]
+  before_action :require_admin, only: [:admin]
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def record_not_found
